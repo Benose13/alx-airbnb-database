b865e831206305951020a38b4bb76f60cad4f10e
Benose13 <okoegualebenjamin38@gmail.com>
2025-09-15 17:35:28 +0100

    EXPLAIN ANALYZE

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,12 +1,14 @@
 
 -- ==========================
 -- Users Table Indexes
+-- EXPLAIN ANALYZE
 -- ==========================
 CREATE INDEX idx_users_email ON Users(email);
 CREATE INDEX idx_users_userid ON Users(user_id);
 
 -- ==========================
 -- Bookings Table Indexes
+-- EXPLAIN ANALYZE
 -- ==========================
 CREATE INDEX idx_bookings_guestid ON Bookings(guest_id);
 CREATE INDEX idx_bookings_propertyid ON Bookings(property_id);
@@ -14,6 +16,7 @@ CREATE INDEX idx_bookings_checkin_checkout ON Bookings(check_in, check_out);
 
 -- ==========================
 -- Properties Table Indexes
+-- EXPLAIN ANALYZE
 -- ==========================
 CREATE INDEX idx_properties_location ON Properties(location);
 CREATE INDEX idx_properties_price ON Properties(price);
